b6e6b111ea10ee2d30514dc5cce7e4d7db4762fb
Kirill <kirill.1998@inbox.ru>
2024-02-06 21:51:37 +0300

    отладил многоуровневую структуру

--- tree_menu/templatetags/menu_tags.py
+++ tree_menu/templatetags/menu_tags.py
@@ -8,7 +8,18 @@ register = template.Library()
 def draw_menu(context, menu_name):
     current_url = context['request'].path
     menu_items = MenuItem.objects.filter(parent=None)
+    full_menu = get_full_menu(menu_items, current_url)
+    return {'menu_items': full_menu}
+
+def get_full_menu(menu_items, current_url):
+    full_menu = []
     for item in menu_items:
-        item.active = item.url == current_url
-        item.children = MenuItem.objects.filter(parent=item)
-    return {'menu_items': menu_items}
+        active = item.url == current_url
+        children = get_full_menu(MenuItem.objects.filter(parent=item), current_url)
+        full_menu.append({
+            'name': item.name,
+            'url': item.url,
+            'active': active,
+            'children': children,
+        })
+    return full_menu
